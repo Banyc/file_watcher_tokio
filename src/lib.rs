2fa442c1c8609ba024510568a3e36169539606f0
Banyc <36535895+Banyc@users.noreply.github.com>
2025-05-11 13:46:53 +0800

    refactor: simplify pub trait; reexport `notify`

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-use std::{path::Path, sync::Arc};
+use std::path::Path;
 
 use notify::{RecommendedWatcher, Watcher};
-use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc;
 use tracing::{instrument, trace};
 
+pub use notify::Event;
+
 #[instrument]
-fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<notify::Event>>)>
-{
-    let (tx, rx) = tokio::sync::mpsc::channel(1);
+fn async_watcher() -> notify::Result<(
+    RecommendedWatcher,
+    mpsc::Receiver<notify::Result<notify::Event>>,
+)> {
+    let (tx, rx) = mpsc::channel(1);
 
     let watcher = RecommendedWatcher::new(
         move |res| {
-            futures::executor::block_on(async {
-                tx.send(res).await.unwrap();
-            })
+            tx.blocking_send(res).unwrap();
         },
         notify::Config::default(),
     )?;
@@ -21,15 +23,15 @@ fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Resul
     Ok((watcher, rx))
 }
 
-pub trait EventActor {
-    fn notify(&self, event: notify::Event) -> impl std::future::Future<Output = ()> + Send;
+pub trait HandleEvent {
+    fn handle_event(&mut self, event: notify::Event) -> impl Future<Output = ()> + Send;
 }
 
-#[instrument(skip(actor))]
-pub async fn watch_file<P, EA>(path: P, actor: Arc<EA>) -> notify::Result<()>
+#[instrument(skip(handler))]
+pub async fn watch_file<P, EventHandler>(path: P, mut handler: EventHandler) -> notify::Result<()>
 where
     P: AsRef<Path> + std::fmt::Debug,
-    EA: EventActor,
+    EventHandler: HandleEvent,
 {
     trace!(path = ?path, "Watching file");
 
@@ -37,18 +39,14 @@ where
 
     watcher.watch(path.as_ref(), notify::RecursiveMode::Recursive)?;
 
-    loop {
-        let res = rx.recv().await;
-        let Some(res) = res else {
-            break;
-        };
+    while let Some(res) = rx.recv().await {
         match res {
             Ok(event) => {
-                trace!("File changed: {:?}", event);
-                actor.notify(event).await;
+                trace!("File changed: {event:?}");
+                handler.handle_event(event).await;
             }
             Err(e) => {
-                trace!("Error watching config file: {:?}", e);
+                trace!("Error watching config file: {e:?}");
             }
         }
     }
